5c8b7db0a909470e9234395c9d843c65e949f7df
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-11-17 18:19:41 +0900

    Upload

---
+++ server/1.js
@@ -0,0 +1,61 @@
+var express=require('express')
+var app=express()
+var bodyParser=require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+var mysql=require('mysql')
+
+var connection=mysql.createConnection({
+  host: 192.168.43.32
+  port:3000,
+  user:"root",
+  password:"1234",
+  database:"test"
+})
+
+connection.connect()
+
+app.post('/user', function(req, res){
+  var userID=req.body.id
+  var userPW=req.body.pw
+
+  if(userID && userPW) {
+
+    connection.query("INSERT INTO user(userID, userPW) VALUES (' "+userID +"', '"+userPW+"')",
+        function(error, result, fields){
+
+          if(error){
+            res.send('err: ' +error)
+          }
+          else{
+            console.log(userID + ',' + userPW)
+            res.send('success create user name: '+userID+'pw'+userPW)
+          }
+        })
+  }
+})
+
+app.listen(3000, function(){
+  console.log("server starting with 3000")
+})
+
+var connection=mysql.createConnection({
+  host: 192.168.43.32
+  port:3000,
+  user:"root",
+  password:"1234",
+  batabase:"test"
+})
+
+connection.query("INSERT INTO user (userID, userPW) VALUES ('"+userID+"', '"userPW"')",
+    function(error,result,fields){
+
+      if(error){
+        res.send('err: '+error)
+      }
+      else{
+        console.log(userID + ',' +userPW)
+        res.send('success create user name: '+userID + 'pw: '+ userPW)
+      }
+    })
