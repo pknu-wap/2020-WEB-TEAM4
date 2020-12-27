29a6eaf13ce77487010b068cafbac4f1a81b2125
정현정 <p001215@naver.com>
2020-12-27 23:04:59 +0900

    휴~지

--- client/register/register.js
+++ client/register/register.js
@@ -1,9 +1,14 @@
-fetch("file:///C:/Users/%EC%A0%95%ED%98%84%EC%A0%95/2020-WEB-TEAM4/client/register/register.html",{
-	method : "POST",
+fetch("router.route('/client/register')",{
+    method : "POST",
+    headers:{
+        'Content-Type': 'application/json'
+      },
    	body : JSON.stringify({
-           user_account : this.state.id,
-      	   password : this.state.pswd1
+           
+
+
         })
 })
 .then(res => res.json())
-.then(res => console.log(res))
+.then(response => console.log('Success:', JSON.stringify(response)))
+.catch(error => console.error('Error:', error));
