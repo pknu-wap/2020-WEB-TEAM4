bae25b382141af200a52308ceb2ea230c2e16473
jaeheek1m <bobe0419@naver.com>
2020-12-28 18:28:40 +0900

    .

--- client/write/write_r.js
+++ client/write/write_r.js
@@ -2,42 +2,24 @@ const toggleBtn = document.querySelector('.mainbar_toggleBtn');
 const menu = document.querySelector('.main_menu');
 const login = document.querySelector('.main_login');
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    login.classList.toggle('active');
-});
-var express = require('express');
-var router = express.Router();
-var mysql_odbc = require('../db/db_conn')();
-var conn = mysql_odbc.init();
- 
- 
-router.get('/recommend/:page', function(req, res, next) {
-    var page = req.params.page;
-    var sql = "select idx, name, title, date_format(modidate,'%Y-%m-%d %H:%i:%s') modidate, " +
-        "date_format(regdate,'%Y-%m-%d %H:%i:%s') regdate from board";
-    conn.query(sql, function (err, rows) {
-        if (err) console.error("err : " + err);
-        res.render('recommend', {title: '게시판 리스트', rows: rows});
-    });
-});
-module.exports = router;
+function formCheck() {
 
-router.get('/write', function(req,res,next){
-    res.render('write',{title : "게시판 글 쓰기"});
-});
-
-router.post('/write', function(req,res,next){
-    var name = req.body.name;
-    var title = req.body.title;
-    var content = req.body.content;
-    var passwd = req.body.passwd;
-    var datas = [name,title,content,passwd];
- 
- 
-    var sql = "insert into test(name, title, content, regdate, modidate, passwd,hit) values(?,?,?,now(),now(),?,0)";
-    conn.query(sql,datas, function (err, rows) {
-        if (err) console.error("err : " + err);
-        res.redirect('/test/list');
-    });
-});
+    var length = document.forms[0].length-1;
+    
+    for(var i = 0; i < length; i++){ 
+    
+        if(document.forms[0][i].value == null || document.forms[0][i].value == ""){
+    
+            alert(document.forms[0][i].name + "을/를 입력하세요.");
+    
+            document.forms[0][i].focus();
+            return false
+        }//end if
+    }}
+    $(document).ready(function(){ var fileTarget = $('.filebox .upload-hidden'); fileTarget.on('change', function(){
+        if(window.FileReader){
+            var filename = $(this)[0].files[0].name; } 
+        else { 
+            var filename = $(this).val().split('/').pop().split('\\').pop();
+        }
+        $(this).siblings('.upload-name').val(filename); }); });
