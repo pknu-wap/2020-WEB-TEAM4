d2499ae2afd14400707b9ed65d2e827b4a1ad0b6
정현정 <p001215@naver.com>
2020-12-29 00:09:46 +0900

    .

--- client/login/loginbar.js
+++ client/login/loginbar.js
@@ -6,3 +6,18 @@ toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     login.classList.toggle('active');
 });
+
+function getInfo()
+{
+    var id=document.getElementById("id").Value;
+    var pw=document.getElementById("pw").Value;
+
+    if(id=="admin" && pw=="user"){
+        alert("로그인에 성공하였습니다");
+        return false;
+    }
+    else{
+        alert("로그인에 실패하였습니다");
+    }
+
+}
